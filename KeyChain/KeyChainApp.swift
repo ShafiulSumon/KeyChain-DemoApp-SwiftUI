1aacc5ededdbd50b61e4a4d23310731a6e0f1003
Md_Shafiul_Alam <shafiul.alam@bjitgroup.com>
2023-04-12 12:13:17 +0600

    Initial Commit

---
+++ KeyChain/KeyChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyChainApp.swift
+//  KeyChain
+//
+//  Created by ShafiulAlam-00058 on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
